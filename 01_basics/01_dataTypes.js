ee882eeb9e215792657c24d3b2f0306911c9472b
SravaniEerlapally <103051643+SravaniEerlapally@users.noreply.github.com>
2023-11-20 18:17:23 +0530

    dataTypes

---
+++ 01_basics/01_dataTypes.js
@@ -0,0 +1,17 @@
+"use strict"//treat all js code as newer version
+alert(3+3)
+//not preferred to write as it is not having a high readability
+console.log("hey); console.log(5+2);
+/*DATATYPES
+   PRIMITIIVE
+     NUMBERS->represent both int & float
+     BIGINT->greaterthan numbers size we use
+     STRINGS->" " & ' '
+     BOOLEAN->true,false
+     NULL VALUE->when we want to keep any variable empty we assign null .It's a special value
+     UNDEFINED VALUE->when defined variable not assigned anything it gives
+  NON-PRIMITIVE
+     OBJECTS
+     SYMBOLS */
+console.log(typeof null)//object
+console.log(typeof undefined)//undefined
