6e76d15eeb4d4eccab7ea0c246d055bb2dfa9e15
SravaniEerlapally <103051643+SravaniEerlapally@users.noreply.github.com>
2023-11-20 22:14:43 +0530

    03_conversionOperations.js

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,2 +1,21 @@
-let score = 44
+let score = "55a"
 console.log(typeof score);
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)//o/p->Nan
+/* "55"=> 55
+   "55a" => Nan
+   true=>1; false =>0
+*/
+let isLoggedIn=1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+//1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+let num = 55
+let stringnum = string(num)
+console.log(typeof stringnum);
+console.log(stringnum)
+
+
