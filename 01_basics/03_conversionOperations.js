d05a3047c5ae4f46810d2492cfc99f4318364cdf
SravaniEerlapally <103051643+SravaniEerlapally@users.noreply.github.com>
2023-11-21 16:46:32 +0530

    03_conversionOperations.js

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -17,5 +17,36 @@ let num = 55
 let stringnum = string(num)
 console.log(typeof stringnum);
 console.log(stringnum)
+//*******************Operations******************************//
+let value = 3
+let negValue = -value
+console.log(negValue);
+/*console.log(2**2);
+console.log(2-5);
+console.log(2+5);
+console.log(2/2);
+console.log(2%2);
+console.log(2*3);*/
+let str1 = "hello"
+let str2 = "sravani"
+let str3 = str1 + str2
+console.log(str3)
+console.log(1+2+"3")
+console.log("1"+2)
+console.log(+true);//o/p:1
+console.log(+"");//o/p:0
+
+let num1,num2,num3
+num1 = num2 = num3 =2 + 2//not good for readability
+let gameCounter = 100;
+gameCounter++;//postfix
+++gameCounter;//prefix
+console.log(gameCounter);//101
+//*************COMPARISONS**************//
+//*************DataTypes Summary**********//
+
+
+
+
 
 
