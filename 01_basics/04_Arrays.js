55b5d4151a2738f4e1029b40f0e214abf6e63afb
SravaniEerlapally <103051643+SravaniEerlapally@users.noreply.github.com>
2023-11-22 02:34:45 +0000

    Memory

--- 01_basics/04_Arrays.js
+++ 01_basics/04_Arrays.js
@@ -1,2 +1,15 @@
-//Memory 2 types
-//Stack(Primitive) & Heap(NonPrimitive)
+//Memory->>>Stack(Primitive->copy milta usme change hota ) & Heap(NonPrimitive->original mein change hota reference se)
+let myyou = "SravaniTech"
+let anothername = myyou;
+console.log(anothername);
+anothername="chaiaurcode";
+console.log(anothername);
+console.log(myyou);//stack
+let user1={
+    email:"abc@gmail.com",
+    upi:"df@31"
+
+}
+user2=user1
+user2.email = "sravani@gmail.com"
+console.log(user1.email);//heap>>dono mein value change hota hai
