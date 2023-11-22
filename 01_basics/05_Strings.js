db849f5356831b170d4df6038213fb75b7dfe9cf
SravaniEerlapally <103051643+SravaniEerlapally@users.noreply.github.com>
2023-11-22 03:23:06 +0000

    methods

--- 01_basics/05_Strings.js
+++ 01_basics/05_Strings.js
@@ -2,7 +2,9 @@ const name = "sravani"
 const repoCount = 50
 console.log(name + repoCount + " Value");//not readable
 console.log(`Hello my name is ${name} and my repo Count is ${repoCount}`);//modern way with back strings preferred and high readable
+
 const gameName = new String('SravaniE')
+//***************String Methods*************/
 console.log(gameName[0]);
 console.log(gameName.__proto__);
 console.log(gameName.length);
@@ -11,3 +13,12 @@ console.log(gameName.charAt(4));
 console.log(gameName.indexOf('n'));
 const newString = gameName.substring(0,4)
 console.log(newString);
+const anotherString = gameName.slice(-5,4)
+console.log(anotherString)
+
+const Str1 = "    sravani     "
+console.log(Str1);
+console.log(Str1.trim());
+const url = "sdfghj44@gg.com";
+console.log(url.replace('@g','--@'));
+console.log(url.includes('tt'));
