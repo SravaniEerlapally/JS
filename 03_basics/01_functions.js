ac23acc643b1b4c58e4277d00e613dc883c1a939
SravaniEerlapally <103051643+SravaniEerlapally@users.noreply.github.com>
2023-11-30 11:29:15 +0000

    funcs parameters

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -1,4 +1,28 @@
 function sayMyName(){
     console.log("Sravani");
 }
+//sayMyName->reference ||||sayMyName->calling function
 sayMyName();
+function addTwoNum(num1,num2){
+    console.log(num1+num2);
+}
+addTwoNum(5,7);
+addTwoNum(4,null);
+function addNums(n1,n2){
+    let result = n1 + n2
+    //console.log("Sravani");
+    return result;
+}
+const result = addNums(3,66);
+
+console.log("Result: ",result);
+function loginUserMessage(username){
+    if(!username){
+        console.log("Please Enter your Name");
+        return;
+    }
+    return `${username} just logged in`
+
+}
+console.log(loginUserMessage("Sravani"));
+console.log(loginUserMessage());
