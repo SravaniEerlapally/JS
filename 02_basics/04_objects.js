7b4771786f246abea4072ddcb9eff9267513e737
SravaniEerlapally <103051643+SravaniEerlapally@users.noreply.github.com>
2023-11-30 09:54:34 +0000

    obj2

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -2,7 +2,7 @@ const tinderUser = new Object()//singleton object
 const tinderUser1 = {}//nonsingleton object
 tinderUser.id = "12345";
 tinderUser.name = "Jemmy";
-tinderUser.isLogginedin = false
+tinderUser.isLogginedIn = false
 console.log(tinderUser);
 //console.log(tinderUser1);
 
@@ -28,3 +28,39 @@ console.log(obj4);
 //3rd way mostly used
 const obj5 = {...obj1,...obj2}
 console.log(obj5);
+
+const users = [
+    {
+        id: 1,
+        email: "gfg@yahoo.com"
+    },
+    {
+        id: 2,
+        email: "gfgg@yahoo.com"
+    },
+    {
+        id: 3,
+        email: "gfgf@yahoo.com"
+    },
+    {
+        id: 5,
+        email: "efg@yahoo.com"
+    }
+]
+console.log(users[1].email);
+console.log(Object.keys(tinderUser));//--IMP--all keys output's in array form 
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+console.log(tinderUser.hasOwnProperty('isLogginedIn'))//to check whether present or not
+
+
+
+
+
+
+
+
+
+
+
+
